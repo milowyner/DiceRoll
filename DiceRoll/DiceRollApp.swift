4775af731052d3e0631444f6960935d0e665f928
Milo Wyner <milowyner@gmail.com>
2021-09-30 17:36:34 -0700

    Initial Commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Milo Wyner on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
