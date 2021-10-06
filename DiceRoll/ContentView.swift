5d6b585fd9aaf357afca1dff13261ced867e0ce6
Milo Wyner <milowyner@gmail.com>
2021-10-06 14:27:47 -0700

    Create list of previous rolls. An entry is added after the die is flipped to the last side during the animation

--- DiceRoll/ContentView.swift
+++ DiceRoll/ContentView.swift
@@ -10,14 +10,18 @@ import SwiftUI
 struct ContentView: View {
     private let sides = 6
     @State private var completed = false
+    @State private var previousRolls = [Int]()
     
     var body: some View {
         TabView {
             NavigationView {
                 VStack {
-                    DieView(sides: sides, hapticsEnabled: true, completed: $completed)
-                    DieView(sides: sides, completed: $completed)
-                    DieView(sides: sides, completed: $completed)
+                    DieView(sides: sides, hapticsEnabled: true, completed: $completed) { roll in
+                        if !completed {
+                            previousRolls.insert(roll, at: 0)
+                            completed = true
+                        }
+                    }
                 }
                 .navigationTitle("Dice Roll")
             }
@@ -25,12 +29,16 @@ struct ContentView: View {
                 Image(systemName: "die.face.3")
                 Text("Roll")
             }
-            
-            Text("Hello")
-                .tabItem {
-                    Image(systemName: "dice")
-                    Text("Previous")
+            NavigationView {
+                List(previousRolls, id: \.self) { roll in
+                    Text("\(roll)")
                 }
+                .navigationTitle("Previous Rolls")
+            }
+            .tabItem {
+                Image(systemName: "dice")
+                Text("Previous")
+            }
         }
     }
 }
