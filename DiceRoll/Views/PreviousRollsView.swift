3f9ef843c1eb47e4dafd42d3f3bf4438459e9fa2
Milo Wyner <milowyner@gmail.com>
2021-10-13 12:54:09 -0700

    Add clear button to PreviousRolls which shows a confirmation alert before removing all previous rolls from Core Data

--- DiceRoll/Views/PreviousRollsView.swift
+++ DiceRoll/Views/PreviousRollsView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct PreviousRollsView: View {
+    @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Roll.timestamp, ascending: false)],
         animation: .default)
     private var previousRolls: FetchedResults<Roll>
+    
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -38,7 +41,21 @@ struct PreviousRollsView: View {
                     }
                 }
             }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Clear previous rolls?"),
+                      message: Text("This cannot be undone."),
+                      primaryButton: .cancel(),
+                      secondaryButton: .destructive(Text("Clear"), action: clear))
+            }
             .navigationTitle("Previous Rolls")
+            .navigationBarItems(trailing: Button("Clear") {
+                showingAlert = true
+            }.disabled(previousRolls.isEmpty))
         }
+    }
+    
+    private func clear() {
+        previousRolls.forEach(viewContext.delete)
+        PersistenceController.shared.save()
     }
 }
